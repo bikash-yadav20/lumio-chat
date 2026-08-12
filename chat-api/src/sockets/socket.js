64ba82352f2bddb03d17d641b6da7be7a8df36b9
bikash-yadav20 <nishantyadav2488@gmail.com>
2026-08-12 12:14:10 +0530

    updated some env variabale logic

--- chat-api/src/sockets/socket.js
+++ chat-api/src/sockets/socket.js
@@ -5,7 +5,7 @@ let io;
 const initializeSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.FRONTEND_URL,
       credentials: true,
     },
   });
